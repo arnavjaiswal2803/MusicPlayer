15e1d6ce143fd9ab0f997ba71018c20d836c8aeb
Arnav Jaiswal <arnavjaiswal2803@gmail.com>
2021-10-15 12:26:56 +0530

    Commiting for backup

---
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.musicplayer;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.audio_file);
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Toast.makeText(MainActivity.this, "Song completed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button playButton = (Button) findViewById(R.id.play_button);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Playing song"
+                        , Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
+            }
+        });
+
+        Button pauseButton = (Button) findViewById(R.id.pause_button);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Pausing song"
+                        , Toast.LENGTH_SHORT).show();
+                mediaPlayer.pause();
+            }
+        });
+
+        Button resetButton = (Button) findViewById(R.id.reset_button);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Resetting song to the start"
+                        , Toast.LENGTH_SHORT).show();
+                mediaPlayer.reset();
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.audio_file);
+            }
+        });
+    }
+}
